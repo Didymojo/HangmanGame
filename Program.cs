3c16651f04d0b686f8a46d4192ac7dfec32c6846
Didymojo <claire.nielsen@yahoo.com.au>
2022-07-31 16:52:23 +1000

    edits up to 31.07.22 letters guessed included

--- Program.cs
+++ Program.cs
@@ -2,6 +2,29 @@
 {
     internal class Program
     {
+        private static int printWord(List<char> letterGuess, String randomWord)
+        {
+            int counter = 0;
+            int rightLetters = 0;
+            Console.Write("\r\n");
+            foreach (char c in randomWord)
+            {
+                if (letterGuess.Contains(c))
+                {
+                    Console.Write(c + " ");
+                    rightLetters += 1;
+                }
+                else
+                {
+                    Console.Write("_ ");
+
+                }
+                counter += 1;
+            }
+            return rightLetters;
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hangman");
@@ -17,47 +40,67 @@
             wordList.Add("crown");
 
             int index = rnd.Next(wordList.Count);
-
+            // Console.WriteLine($"this is the index int: {index}");
             string randomWord = wordList[index];
-            Console.WriteLine($"the random word is: {rnd}");
+            Console.WriteLine($"the random word is: {randomWord}");
 
-            Console.WriteLine("type your letter guess:");
-            string playerGuess = Console.ReadLine();
-            Console.WriteLine($"The user chose: {playerGuess}");
-
-            //if (rnd.Contains(playerGuess) == true)  // String.Contains() method returns a value indicating whether a specified substring occurs within this string
-            //{
-            //    Console.WriteLine("letter found");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("letter not found");
-            //}
-
-
-            //    Console.ReadKey();//not sure about how am supposed to use this - it returns the player guess
-
-            int counter = 0;
-            int rightLetters = 0;
-
-            foreach (char c in randomWord)
+            foreach (char x in randomWord)
             {
-                if (playerGuess.Contains(c))
+                Console.Write("_ ");
+            }
+
+            int numTimesWrong = 0;
+            int lengthOfTargetWord = randomWord.Length;
+            List<char> lettersAlreadyGuessed = new List<char>();
+            int lettersGotRight = 0;
+            while (numTimesWrong != 6 && lettersGotRight != lengthOfTargetWord)
+            {
+                Console.Write("\r\nLetters guessed so far: ");
+
+                foreach (char c in lettersAlreadyGuessed)
                 {
                     Console.Write(c + " ");
-                    rightLetters += 1;
+                }
+
+                Console.Write("\nGuess a letter: \n");
+                char letterGuess = Console.ReadLine()[0];
+
+                //check if user has already guessed that letter
+                if (lettersAlreadyGuessed.Contains(letterGuess))
+                {
+                    Console.Write("\r\nYou have already guessed this letter");//\r - carriage return, moves cursor back to beginning of current line
+                    lettersGotRight = printWord(lettersAlreadyGuessed, randomWord);
+                    Console.Write("\r\n");
                 }
                 else
                 {
-                    Console.Write("- ");
+                    // Check if letter is in randomWord
+                    bool right = false;
+                    for (int i = 0; i < randomWord.Length; i++)
+                    {
+                        if (letterGuess == randomWord[i])
+                        {
+                            right = true;
+                        }
+                    }
+
+                    if (right)
+                    {
+                        lettersAlreadyGuessed.Add(letterGuess);
+                        lettersGotRight = printWord(lettersAlreadyGuessed, randomWord);
+                        Console.Write("\r\n");
+                    }
+                    else //are wrong
+                    {
+                        numTimesWrong += 1;
+                        lettersAlreadyGuessed.Add(letterGuess);
+                        lettersGotRight = printWord(lettersAlreadyGuessed, randomWord);
+                        Console.Write("\r\n");
+                    }
                 }
-                counter += 1;
-
-
-
             }
+            Console.WriteLine("\r\nGame over! Thanks for playing");
         }
     }
 }
 
-//    Console.ReadKey();//not sure about how am supposed to use this - it returns the player guess
